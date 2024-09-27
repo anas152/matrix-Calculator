7f3cba0a0b4da29ebdb3bfab93d0838865c04fb4
Muhammad Salman <Salman7official7@gmail.com>
2024-09-28 00:51:56 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 
 
-// let plus = document.getElementById("Plus").value;
-// let minus = document.getElementById("minus").value;
+
 let num1 = document.getElementById("input1")//.value;
 let num2 = document.getElementById("input2")//.value;
 let inputsDiv1 = document.getElementById("inputdiv1");
 let inputsDiv2 = document.getElementById("inputdiv2");
 let resultDiv = document.getElementById("resultDiv");
-// let rows;
-// let columns;
+
 
 
 function matricesCal() {
@@ -36,17 +34,15 @@ for(let i = 0; i<num1; i++){
     for(let j=0; j<num2; j++){
   let value1 = document.getElementById(`inputbox1-${i}${j}`).value
   let value2 = document.getElementById(`inputbox2-${i}${j}`).value
-console.log(value1)
-  value1 = parseInt(value1)
-  value2 = parseInt(value2)
-  
- let result = value1 + value2;
-//  console.log(result)
+// console.log(value1)
+  let add = parseInt(value1) + parseInt(value2);
+//  console.log(add)
 
-resultDiv.innerHTML +=` <input type="number" id="resultDiv-${i}${j}" style="width: 30px;" value = ${i}${j}`;
+let res = document.querySelector(`#resultDiv-${i}${j}`).value = add;
+// console.log(res);
 
     }
-  resultDiv.innerHTML += "<br>"
+  resultDiv.innerHTML += "<br />>"
   }
 
 
@@ -58,104 +54,3 @@ resultDiv.innerHTML +=` <input type="number" id="resultDiv-${i}${j}" style="widt
 
 
  
-// if (input1 === input2){
-//     const calculating = input1 * input2;
-//     console.log(calculating)
-//     let inputTag = document.createElement("input");
-//     // console.log(inputTag);
-//       matrix1 = matrix1.innerHTML = inputTag
-//     console.log(matrix1);
-
-// }
-
-
-
-
-
-// function calculateMatrix() {
-//     const num1 = parseInt(document.getElementById("num1").value);
-//     const num2 = parseInt(document.getElementById("num2").value);
-//     const operation = document.getElementById("operation").value;
-  
-//     // Matrix create karna
-//     const matrix1 = [
-//       [num1, 0],
-//       [0, num1]
-//     ];
-//     const matrix2 = [
-//       [num2, 0],
-//       [0, num2]
-//     ];
-  
-//     // Operation apply karna
-//     let result;
-//     if (operation === "+") {
-//       result = [
-//         [matrix1[0][0] + matrix2[0][0], matrix1[0][1] + matrix2[0][1]],
-//         [matrix1[1][0] + matrix2[1][0], matrix1[1][1] + matrix2[1][1]]
-//       ];
-//     } else if (operation === "-") {
-//       result = [
-//         [matrix1[0][0] - matrix2[0][0], matrix1[0][1] - matrix2[0][1]],
-//         [matrix1[1][0] - matrix2[1][0], matrix1[1][1] - matrix2[1][1]]
-//       ];
-//     }
-  
-//     // Result display karna
-//     const resultDiv = document.getElementById("result");
-//     resultDiv.innerHTML = `
-//       Matrix 1: <br>
-//       ${matrix1[0][0]} ${matrix1[0][1]} <br>
-//       ${matrix1[1][0]} ${matrix1[1][1]} <br>
-//       <br>
-//       Matrix 2: <br>
-//       ${matrix2[0][0]} ${matrix2[0][1]} <br>
-//       ${matrix2[1][0]} ${matrix2[1][1]} <br>
-//       <br>
-//       Result: <br>
-//       ${result[0][0]} ${result[0][1]} <br>
-//       ${result[1][0]} ${result[1][1]}
-//     `;
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function matricCal() {
-
-//     let plus = document.getElementById("Plus").value;
-//      let minus = document.getElementById("minus").value;
-//     let input1 = document.getElementById("input1").value;
-//     let input2 = document.getElementById("input2").value;
-
-
-// if(input1 === input2){
-//     let equal = input1 * inp                                                                                                                                                                                                                                                    zxut2
-//     if((plus.value) === "+"){
-//    let createInp = document.createElement("input");
-// console.log(createInp.value);
-
-
-//     }
-// }else {
-//     alert("Please enter a same number");
-// }
-
-//  }
